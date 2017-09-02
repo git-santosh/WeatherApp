2bdaa0ca5f9d576ae15adec97e389464053f92df
santosh suryawanshi <santosh.suryawanshi28@gmail.com>
2017-09-02 10:49:38 +0530

    weather search by city name

--- controller/weatherController.js
+++ controller/weatherController.js
@@ -1,12 +1,23 @@
 const fs = require('fs');
 const axios = require('axios');
 require('dotenv').config({path:'.env'});
+
 exports.homePage = (req,res) =>{
     axios.get(process.env.watherURL+'pune'+'&APPID='+process.env.APPID+'&units='+process.env.units.split(',')[0]).then(function(response){
-        var img = "http://openweathermap.org/img/w/" + response.data.weather[0].icon.toString() + ".png"
+      var img = "http://openweathermap.org/img/w/" + response.data.weather[0].icon.toString() + ".png"
       res.render('index',{data:response.data ,iconCode : img});
     }).catch(function(err){
-        
+        res.send({error:err , failure:true});
     });
   //  res.render('index');
+}
+
+exports.showCity =(req,res) =>{
+      let city =req.body.cityName;
+      axios.get(process.env.watherURL+city+'&APPID='+process.env.APPID+'&units='+process.env.units.split(',')[0]).then(function(response){
+        var img = "http://openweathermap.org/img/w/" + response.data.weather[0].icon.toString() + ".png"
+        res.send({ajx:response.data ,iconCode : img});
+    }).catch(function(err){
+        res.send({error:err});
+    });
 }
--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,5 @@ const weatherController = require('../controller/weatherController');
 //get home page 
 
 router.get('/',weatherController.homePage);
-
+router.post('/',weatherController.showCity);
 module.exports =router;
